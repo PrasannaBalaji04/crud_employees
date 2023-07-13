cd10589e54eb20a7b41ef18947ce2ea33ca8a81b
PrasannaBalaji04 <prasannabalaji.rangarajan@bounteous.com>
2023-07-13 10:13:05 +0530

    validate user _id

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,4 +1,5 @@
 const Employee = require('../models/employee');
+var mongoose = require('mongoose');
 
 // Add employee details to the database
 async function addEmployee(req, res) {
@@ -19,24 +20,35 @@ async function getEmployees(req, res) {
     res.status(500).json({ success: false, error: error.message });
   }
 }
+//get employee
 async function getEmployee(req, res) {
   try {
-    const employees = await Employee.findById(req.params.id);
-    res.json({ success: true, data: employees });
+    console.log(req.params.id);
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+      res.status(400).json({ success: false, error: "Id field is not valid" });
+    }else{
+      const employees = await Employee.findById(req.params.id);
+      res.json({ success: true, data: employees });
+    }
   } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
+    res.status(400).json({ success: false, error: error.message });
   }
 }
 // Update employee details
 async function updateEmployee(req, res) {
   try {
-    const employee = await Employee.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-    res.json({ success: true, data: employee });
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+      res.status(400).json({ success: false, error: "Id field is empty" });
+    }else{
+      const employee = await Employee.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      res.json({ success: true, data: employee });
+    }
   } catch (error) {
+    console.log(error.message);
     res.status(500).json({ success: false, error: error.message });
   }
 }
@@ -44,12 +56,19 @@ async function updateEmployee(req, res) {
 // Delete an employee
 async function deleteEmployee(req, res) {
   try {
+    if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+    console.log("Id is empty");
+    res.status(400).json({ success: false, error: "Id field is empty"});
+  }else{
     await Employee.findByIdAndRemove(req.params.id);
     res.json({ success: true, message: 'Employee deleted successfully' });
+  }
   } catch (error) {
+    console.log(error.message);
     res.status(500).json({ success: false, error: error.message });
   }
 }
+
 
 module.exports = {
   addEmployee,
